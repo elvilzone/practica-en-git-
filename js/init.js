be79df285c8bb77434b56ebc32cbcd6e75ac6c9f
valdemar <amigo_diablito@hotmail.com>
2017-04-28 18:52:47 -0400

    mi commit

--- js/init.js
+++ js/init.js
@@ -1,7 +1,10 @@
-(function($){
+/*(function($){
   $(function(){
 
     $('.button-collapse').sideNav();
 
   }); // end of document ready
-})(jQuery); // end of jQuery name space
+})(jQuery); // end of jQuery name space*/
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script> 
+<script src="js/materialize.js"></script> 
+<script src="js/init.js"></script> 
